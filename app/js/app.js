69f5ed62afabcbad54360feabae7665ef1f42859
ahaurw01 <aaron.haurwitz@gmail.com>
2013-08-11 18:31:29 -0400

    first attempt at disqus integration

--- app/js/app.js
+++ app/js/app.js
@@ -78,3 +78,32 @@ Ember.Handlebars.registerBoundHelper('markdown', function (input) {
 Ember.Handlebars.registerBoundHelper('date', function (date) {
   return moment(date).format('MMMM Do, YYYY');
 });
+
+App.DisqusView = Ember.View.extend({
+  elementId: 'disqus_thread',
+  tagName: 'div',
+  didInsertElement: function () {
+    var existingScript = document.getElementById('disqus_script');
+    if (existingScript) { // Simply reload disqus
+      var title = this.get('post.id');          
+      DISQUS.reset({
+        reload: true,
+        config: function () {  
+          this.page.identifier = title;
+          this.page.url = window.location.toString();
+        }
+      });
+    } else {
+      window.disqus_shortname = 'haurwitz';
+      window.disqus_identifier = this.get('post.id');
+      window.disqus_url = window.location.toString();
+      (function() {
+        var dsq = document.createElement('script');
+        dsq.type = 'text/javascript';
+        dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+      })();
+    }
+  }
+});
