beeb32bf5210c1780deb8027b89784db629944f3
ahaurw01 <aaron.haurwitz@gmail.com>
2013-08-24 09:00:49 -0400

    Clean up the DisqusView a little

--- app/js/app.js
+++ app/js/app.js
@@ -86,8 +86,7 @@ App.DisqusView = Ember.View.extend({
   elementId: 'disqus_thread',
   tagName: 'div',
   didInsertElement: function () {
-    var existingScript = document.getElementById('disqus_script');
-    if (existingScript) { // Simply reload disqus
+    if (window.DISQUS) { // Simply reload disqus
       var id = this.get('post.id'),
           title = this.get('post.title');        
       DISQUS.reset({
@@ -103,14 +102,11 @@ App.DisqusView = Ember.View.extend({
       window.disqus_identifier = this.get('post.id');
       window.disqus_url = window.location.toString();
       window.disqus_title = this.get('post.title');
-      (function() {
-        var dsq = document.createElement('script');
-        dsq.type = 'text/javascript';
-        dsq.async = true;
-        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-        dsq.id = 'disqus_script';
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      })();
+      var dsq = document.createElement('script');
+      dsq.type = 'text/javascript';
+      dsq.async = true;
+      dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     }
   }
 });
